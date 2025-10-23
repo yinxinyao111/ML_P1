65cebb034f81f00c04af161de174ca1ecee04225
yinxinyao111 <i@xinyaoyin.ru>
2025-10-23 12:55:28 -0400

    Encoder Decoder blocks in progress

--- model.py
+++ model.py
@@ -119,4 +119,37 @@ class MultiHeadAttentionBlock(nn.Module):
 # ----------------------------------------------------------
 
 class EncoderBlock(nn.Module):
-    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block)
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(2)])
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, features: int, layers: nn.Module):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x)
+        return self.norm(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+    
