b82c059c79a98428e50ee2789867c9a616dce800
yinxinyao111 <i@xinyaoyin.ru>
2025-10-22 21:09:16 -0400

    model.py in progress

--- model.py
+++ model.py
@@ -3,4 +3,50 @@ import torch.nn as nn
 import math
 
 class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    def forward(self, x):
+        # (batch, seq_len) -> (batch, seq_len, d_model)
+        return self.embedding(x) * math.sqrt(x)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # result holder (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # pos (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype = float).unsqueeze(1) 
+        # div_term (d_model/2)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+        # apply sine to even indices (seq_len, d_model/2)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # apply cos to odd indices (seq_len, d_model/2)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        # add batch dimension to pe (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)
+        # resigter pe as buffer
+        self.register_buffer("pe", pe)
+    def forward(self, x):
+        # (batch, seq_len, d_model)
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
     
+class LayerNormalization(nn.Module):
+    def __init__(self, features: int, eps: float = 10**-6):
+        super.__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # (d_model)
+        self.bias = nn.Parameter(torch.zeros(features)) # (d_model)
+    def forward(self, x):
+        # (batch, seq_len, 1)
+        mean = x.mean(dim = -1, keepdim = True)
+        # (batch, seq_len, 1)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
