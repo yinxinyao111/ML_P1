b056b6a0ff7ff6fddbb0b39be351021172cf3f43
yinxinyao111 <i@xinyaoyin.ru>
2025-10-23 10:04:35 -0400

    model.py rescon added

--- model.py
+++ model.py
@@ -50,3 +50,14 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim = True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class ResidualConnection(nn.Module):
+    
